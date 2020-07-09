2813e400deac299c81df13d5ff69b7740625afdf
Seth W <swidoff@gmail.com>
2020-07-08 20:41:36 -0700

    Updated to use a heap-allocated stack.

--- initial-reverse/src/main.rs
+++ initial-reverse/src/main.rs
@@ -1,25 +1,25 @@
 use std::env;
 use std::fs::File;
 use std::io::{Bytes, Read, stdin, stdout, Write};
-use std::process::exit;
 use std::os::linux::fs::MetadataExt;
+use std::process::exit;
 
 use itertools::Itertools;
+use std::rc::Rc;
 
 fn main() {
-
     let file_names = env::args().skip(1).collect_vec();
     match file_names.len() {
         0 => {
             let in_file = stdin();
             let mut out_file = stdout();
             write_lines_reversed(&mut out_file, collect_lines(in_file));
-        },
+        }
         1 => {
             let in_file = open_file(&file_names[0]);
             let mut out_file = stdout();
             write_lines_reversed(&mut out_file, collect_lines(in_file));
-        },
+        }
         2 => {
             let in_file = open_file(&file_names[0]);
             if is_the_same_file_as(&file_names[1], &in_file) {
@@ -28,7 +28,7 @@ fn main() {
             }
             let mut out_file = create_file(&file_names[1]);
             write_lines_reversed(&mut out_file, collect_lines(in_file));
-        },
+        }
         _ => {
             eprintln!("usage: reverse <input> <output>");
             exit(1);
@@ -84,12 +84,44 @@ fn read_line<R: Read>(b_iter: &mut Bytes<R>) -> Option<String> {
     }
 }
 
-fn collect_lines<'a, R: Read + 'a>(read: R) -> Vec<String> {
-    read.bytes().batching(|b_iter| read_line(b_iter)).collect_vec()
+fn collect_lines<'a, R: Read + 'a>(read: R) -> Stack<String> {
+    read.bytes()
+        .batching(|b_iter| read_line(b_iter))
+        .fold(Stack::new(), |stack, v| stack.push(v))
 }
 
-fn write_lines_reversed<'a, W: Write + 'a>(write: &mut W, lines: Vec<String>) {
-    for line in lines.iter().rev() {
+fn write_lines_reversed<'a, W: Write + 'a>(write: &mut W, lines: Stack<String>) {
+    let mut head = lines;
+    while let Some((line, new_head)) = head.pop() {
         write.write(line.as_bytes()).expect("Unable to write line.");
+        head = new_head;
+    }
+}
+
+enum StackElement<T> {
+    Cons(T, Rc<StackElement<T>>),
+    Nil,
+}
+
+struct Stack<T> {
+    head: Rc<StackElement<T>>
+}
+
+impl<T> Stack<T> {
+    fn new() -> Stack<T> {
+        return Stack { head: Rc::new(StackElement::Nil) };
+    }
+
+    fn push(&self, value: T) -> Stack<T> {
+        return Stack { head: Rc::new(StackElement::Cons(value, Rc::clone(&self.head))) };
+    }
+
+    fn pop(&self) -> Option<(&T, Stack<T>)> {
+        match &*self.head {
+            StackElement::Cons(value, tail) => {
+                Some((value, Stack { head: Rc::clone(&tail) }))
+            }
+            StackElement::Nil => None
+        }
     }
 }
